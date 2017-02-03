21cf64aae4457f55b9bd63b7c8bf465ad7ec6f3b
cbw36 <cbw36@case.edu>
2017-02-03 12:05:21 -0500

    Update for lab 1 to move in a square

--- src/stdr_open_loop_commander.cpp
+++ src/stdr_open_loop_commander.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
     //some "magic numbers"
     double sample_dt = 0.01; //specify a sample period of 10ms  
     double speed = 0.4; // 0.4 m/s speed command - adjusted from original program
-    double yaw_rate = 0.5; //0.5 rad/sec yaw rate command
+    double yaw_rate = 0.523; //0.5 rad/sec yaw rate command
     double time_3_sec = 3.0; // should move 3 meters or 1.5 rad in 3 seconds
     
       
@@ -28,6 +28,7 @@ int main(int argc, char **argv) {
       twist_commander.publish(twist_cmd);
       loop_timer.sleep();
     }
+    
     twist_cmd.linear.x=speed; //command to move forward
     while(timer<time_3_sec) {
           twist_commander.publish(twist_cmd);
@@ -44,168 +45,53 @@ int main(int argc, char **argv) {
           }
 
     twist_cmd.angular.z=0.0; //and stop spinning in place 
-    twist_cmd.linear.x=speed; //and move forward again
+    twist_cmd.linear.x=speed; //command to move forward
+    while(timer<time_3_sec) {
+          twist_commander.publish(twist_cmd);
+          timer+=sample_dt;
+          loop_timer.sleep();
+          }
+    twist_cmd.linear.x=0.0; //stop moving forward
+    twist_cmd.angular.z=yaw_rate; //and start spinning in place
     timer=0.0; //reset the timer
     while(timer<time_3_sec) {
           twist_commander.publish(twist_cmd);
           timer+=sample_dt;
           loop_timer.sleep();
           }
+    
+    twist_cmd.angular.z=0.0; //and stop spinning in place 
+    twist_cmd.linear.x=speed; //command to move forward
+    while(timer<time_3_sec) {
+          twist_commander.publish(twist_cmd);
+          timer+=sample_dt;
+          loop_timer.sleep();
+          }
+    twist_cmd.linear.x=0.0; //stop moving forward
+    twist_cmd.angular.z=yaw_rate; //and start spinning in place
+    timer=0.0; //reset the timer
+    while(timer<time_3_sec) {
+          twist_commander.publish(twist_cmd);
+          timer+=sample_dt;
+          loop_timer.sleep();
+          }
+    
+    twist_cmd.angular.z=0.0; //and stop spinning in place 
+    twist_cmd.linear.x=speed; //command to move forward
+    while(timer<time_3_sec) {
+          twist_commander.publish(twist_cmd);
+          timer+=sample_dt;
+          loop_timer.sleep();
+          }
+   
+    
     //halt the motion
     twist_cmd.angular.z=0.0; 
     twist_cmd.linear.x=0.0; 
     for (int i=0;i<10;i++) {
       twist_commander.publish(twist_cmd);
       loop_timer.sleep();
+        
     }               
-    // Above is sample code; the new code begins here.  .................................................
-    
-
-    twist_cmd.linear.x=speed; //command to move forward
-    while(timer<time_3_sec) {
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }
-
-
-
-    twist_cmd.linear.x=speed; //command to move forward
-    while(timer<time_3_sec) {
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }
-
-    twist_cmd.linear.x=0.0; //stop moving forward
-    twist_cmd.angular.z=yaw_rate; //and start spinning in place
-    timer=0.0; //reset the timer
-    while(timer<9.5){ //Turn 270 degrees instead of 90...
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }
-twist_cmd.angular.z=0.0; //and stop spinning in place 
-    twist_cmd.linear.x=speed; //and move forward again - to the right. Eastward. 
-    timer=0.0; //reset the timer
-    while(timer<3.4) {
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }
-    //halt the motion
-
-    twist_cmd.linear.x=0.0; //stop moving forward
-    twist_cmd.angular.z=yaw_rate; //and start spinning in place
-    timer=0.0; //reset the timer
-    while(timer<3) { //90 deg turn to go north, above the thick black bar
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }
-
-    twist_cmd.angular.z=0.0; //and stop spinning in place 
-    twist_cmd.linear.x=speed; //and move forward again, northward
-    timer=0.0; //reset the timer
-    while(timer<2.1) { //move forward
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }      
-    //halt the motion
-
-    twist_cmd.linear.x=0.0; //stop moving forward
-    twist_cmd.angular.z=yaw_rate; //and start spinning in place
-    timer=0.0; //reset the timer
-    while(timer<2.6) { //Turn to go west, above the thick black bar - manually tuned
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }
-
-    twist_cmd.angular.z=0.0; //and stop spinning in place 
-    twist_cmd.linear.x=speed; //and move forward again, westward
-    timer=0.0; //reset the timer
-    while(timer<1) { //move forward
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }      
-    //halt the motion
-
-
-   twist_cmd.linear.x=0.0; //stop moving forward
-    twist_cmd.angular.z=yaw_rate; //and start spinning in place
-    timer=0.0; //reset the timer
-    while(timer<.75) { //slight course correction...
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }
-
-    twist_cmd.angular.z=0.0; //and stop spinning in place 
-    twist_cmd.linear.x=speed; //and move forward again
-    timer=0.0; //reset the timer
-    while(timer<3.1) {
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }      
-    //halt the motion
-
-   twist_cmd.linear.x=0.0; //stop moving forward
-    twist_cmd.angular.z=yaw_rate; //and start spinning in place
-    timer=0.0; //reset the timer
-    while(timer<11.5) { //Turning back north by northwest...
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }
-
-    twist_cmd.angular.z=0.0; //and stop spinning in place 
-    twist_cmd.linear.x=speed; //and move forward again
-    timer=0.0; //reset the timer
-    while(timer<2.1) {
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }      
-    //halt the motion
-   
-        twist_cmd.linear.x=0.0; //stop moving forward
-    twist_cmd.angular.z=yaw_rate; //and start spinning in place
-    timer=0.0; //reset the timer
-    while(timer<11){ //Another ~360 degree turn...
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }
-twist_cmd.angular.z=0.0; //and stop spinning in place 
-    twist_cmd.linear.x=speed; //and straight on 'til morning...!
-    timer=0.0; //reset the timer
-    while(timer<3.2) {
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }
-
-// By this point, the 'bot may already be at the top corner; but just in case, we'll have it turn a bit CW and go forward. 
-    twist_cmd.linear.x=0.0; //stop moving forward
-    twist_cmd.angular.z=yaw_rate; //and start spinning in place
-    timer=0.0; //reset the timer
-    while(timer<11.85){ //Another ~360 degree turn...
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }
-twist_cmd.angular.z=0.0; //and stop spinning in place 
-    twist_cmd.linear.x=speed; //and straight on 'til morning...!
-    timer=0.0; //reset the timer
-    while(timer<3) {
-          twist_commander.publish(twist_cmd);
-          timer+=sample_dt;
-          loop_timer.sleep();
-          }
-    //halt the motion
 }
 
